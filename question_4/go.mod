b81e980a5d9cdd4843ba3dfa9a3c73e564da0550
Kingsley <kingholyhill@gmail.com>
2021-08-19 20:16:07 +0100

    done with the ui

---
+++ question_4/go.mod
@@ -0,0 +1,12 @@
+module hng-internship-task2/question_4
+
+go 1.16
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
+	github.com/gin-gonic/gin v1.7.4
+	github.com/heroku/x v0.0.31
+	github.com/spf13/viper v1.8.1
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1
+)
--- question_4/index.go
+++ question_4/index.go
@@ -7,26 +7,137 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"github.com/gin-gonic/contrib/static"
+	"net/smtp"
+
+	// "crypto/tls"
+	/* "github.com/gin-gonic/contrib/static" */
 	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/spf13/viper"
+	// gomail "gopkg.in/mail.v2"
 )
 
-/* func main() {
-    r := gin.Default()
-
-    r.GET("/hello", func(c *gin.Context) {
-        c.String(200, "Hello, World!")
-    })
-
-	r.Use(static.Serve("/", static.LocalFile("./", true)))
-    r.Run()
-} */
+// const myEmail string = "testyholyhill@gmail.com"
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "80"
+		log.Fatal("$PORT set to 80")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("index.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.POST("/", contactMe)
+
+	router.Run(":" + port)
+}
+
+/* func formResponse(c *gin.Context) {
+	w := c.Writer
+
+	fmt.Fprintf(w, 
+		`<html>
+            <head>
+            </head>
+            <body>
+            <h1>Go Timer (ticks every second!)</h1>
+            <div id="output"></div>
+            <script type="text/javascript">
+            console.log("`+hello+`");
+            </script>
+            </body>
+        </html>`)
+} */
+
+func contactMe(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+	
+	script := `<html>
+					<body>
+						<script type="text/javascript">
+							alert("Thank you for contacting me, your email has been received");
+						</script>
+					</body>
+			    </html>`
+
+	r := c.Request
+	w := c.Writer
+
+	if r.Method == "POST" {
+		firstName := c.PostForm("first-name")
+		lastName := c.PostForm("last-name")
+		email := c.PostForm("email")
+		comment := c.PostForm("comment")
+
+		sendMail(email, "Hello " + firstName + " " +  lastName + ". Thank you for reaching out to me.")
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		io.WriteString(w, script)
+
+		fmt.Fprintf(w, "Hello, %q <br>first name:%s <br>last name: %s <br>email: %s <br>comment: %s ", html.EscapeString(r.URL.Path), firstName, lastName, email, comment)
+	}  
+}
+
+func sendMail(email string, body string) {
+	myEmail := getEnv("EMAIL")
+	password := getEnv("PASSWORD")
+
+  	// Receiver email address.
+	to := []string {email}
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+	
+	// Authentication.
+	auth := smtp.PlainAuth("", myEmail, password, smtpHost)
+	
+	// Message.
+	message := []byte(body)
+
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, myEmail, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
+}
+
+func getEnv(key string) string {
+	viper.SetConfigFile(".env")
+  
+	err := viper.ReadInConfig()
+  
+	if err != nil {
+	  log.Fatalf("Error while reading config file %s", err)
+	}
+  
+	value, ok := viper.Get(key).(string)
+  
+	if !ok {
+	  log.Fatalf("Invalid type assertion")
+	}
+  
+	return value
+}
+  
+
+/* func main() {
 	port := GetPort()
 	http.HandleFunc("/", hello)
 	log.Print("Listening on :" + port)
@@ -46,4 +157,4 @@ func GetPort() string {
 	}
 
 	return ":" + port
-}
+} */
